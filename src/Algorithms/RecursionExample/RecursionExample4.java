c4c3d62a4e391646f5dbb6893a328893246aa582
Hiral Suthar <hiralsuthar1@Hirals-MacBook-Pro.local>
2020-07-30 22:15:58 -0700

    Few Old Updates

--- src/Algorithms/RecursionExample/RecursionExample4.java
+++ src/Algorithms/RecursionExample/RecursionExample4.java
@@ -15,9 +15,32 @@ public class RecursionExample4 {
         }
     }
 
+    public static int printFibo2(int count){
+        if(count == 0){
+            return 0;
+        }
+        else if(count == 1){
+            return 1;
+        }
+        return printFibo2(count - 1) + printFibo2(count - 2);
+    }
+
+    public static int totalFibo(int count){
+        if(count <= 1){
+            return count;
+        }
+        return totalFibo(count - 1) + totalFibo(count - 2);
+    }
+
     public static void main(String args[]){
         int count = 15;
-        System.out.print(n1+" "+n2);
-        printFibo(count-2);
+        //System.out.print(n1+" "+n2);
+        //printFibo(count);
+        for (int i=0; i<count; i++) {
+            System.out.print(printFibo2(i)+" ");
+        }
+
+        System.out.println();
+        System.out.println(totalFibo(count));
     }
 }
